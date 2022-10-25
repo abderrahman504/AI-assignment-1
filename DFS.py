ab113780904e0db0c56d06d60c0bb9fbc8f78dfc
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2022-10-25 03:26:16 +0200

    bfs and dfs modified

--- DFS.py
+++ DFS.py
@@ -1,8 +1,11 @@
 import Utility as u
 import time
+from collections import deque
+
 def dfs(intial_state):
     start_time = time.time()
-    frontier = []
+    frontier = deque()
+    frontier_map = set()
     frontier.append(u.State(intial_state, 0))
     explored = set()
     max_depth = 0
@@ -10,18 +13,18 @@ def dfs(intial_state):
         state = frontier.pop()
         puzzle_order = int(state.get_state())
         depth = state.get_cost()
-        max_depth = max(depth, max_depth)
-
-        if not puzzle_order in explored:
-            explored.add(puzzle_order)
+        explored.add(puzzle_order)
 
         if state.is_goal():
-            return True, state.get_path(), depth, len(explored), time.time() - start_time
+            return True, state.get_path(), max_depth, len(explored), time.time() - start_time
 
         neighbors = state.get_child_states()
+
         for neighbor in neighbors:
-            if neighbor not in explored:
+            if neighbor not in explored and neighbor not in frontier_map:
                 neighbor_state = u.State(neighbor, depth + 1, state)
                 frontier.append(neighbor_state)
+                max_depth = max(depth + 1, max_depth)
+                frontier_map.add(neighbor)
+    return False, [], max_depth, len(explored), time.time() - start_time
 
-    return False, [], depth, len(explored), time.time() - start_time
--- bfs.py
+++ bfs.py
@@ -1,27 +1,31 @@
 import Utility as u
 import time
+from collections import deque
+
 def bfs(intial_state):
 	start_time = time.time()
-	queue = []
+	queue = deque()
 	queue.append(u.State(intial_state, 0))
+	queue_map = set()
+	queue_map.add(int(intial_state))
 	explored = set()
 	max_depth = 0
 	while queue:
-		state = queue.pop(0)
+		state = queue.popleft()
 		puzzle_order = int(state.get_state())
 		depth = state.get_cost()
-		max_depth = max(depth, max_depth)
 		if not puzzle_order in explored:
 			explored.add(puzzle_order)
 
 		if state.is_goal():
-			return True, state.get_path(), depth, len(explored), time.time() - start_time
+			return True, state.get_path(), max_depth, len(explored), time.time() - start_time
 
 		neighbors = state.get_child_states()
 		for neighbor in neighbors:
-			if neighbor not in explored:
+			if neighbor not in explored and neighbor not in queue_map:
 				neighbor_state = u.State(neighbor, depth + 1, state)
+				max_depth = max(max_depth, depth+1)
 				queue.append(neighbor_state)
+				queue_map.add(neighbor)
 
-	return False, [], depth, len(explored), time.time() - start_time
-
+	return False, [], max_depth, len(explored), time.time() - start_time
